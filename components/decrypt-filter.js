6a9ccaa2b786fe0eeba487a0d994347bd9896c7d
tomalatv <toni.latvakoski@gmail.com>
2015-10-24 16:58:24 +0300

    added functionality to use multible keys for encrypting messages at decrypt-filter

--- components/decrypt-filter.js
+++ components/decrypt-filter.js
@@ -1,18 +1,43 @@
 'use strict';
 
 angular.module('myApp.decrypt-filter', [])
-
+/**
+* decryption filter for decrypt crypted messages at application message view
+* @param msg crypted message string
+* @param cryptKey key string for encrypt messages can be contain multiple key separated by space
+* @return crypted/encrypted message string depence on crypto key value 
+*/
 .filter('decrypt', [ function() {
   return function(msg, cryptKey) {
-  	if(cryptKey !== undefined &&  cryptKey !== '' 
-			&& msg !== undefined && msg !== '') {
-			var dec = CryptoJS.AES.decrypt(msg, cryptKey);
+  	var cryptKeys = [];
+
+  	if(cryptKey !== undefined &&  cryptKey !== '' && 
+  	    msg !== undefined && msg !== '') {
+			var dec = '';
+		  	
+		  	if (cryptKey.indexOf(' ') !== -1){
+		  		cryptKeys = cryptKey.split(' ');
+		  	}
+  			
+  			if(cryptKeys.length >1){
+  				for (var i = 0; i < cryptKeys.length; i++){
+					dec = CryptoJS.AES.decrypt(msg, cryptKeys[i].trim());
+					if(dec.toString() !== ''){
+						break;
+					}  					
+  				}
+  			} else {
+				dec = CryptoJS.AES.decrypt(msg, cryptKey);
+  			}
+
 			if(dec.toString() !== '' && dec.toString() !== undefined){
 				return dec.toString(CryptoJS.enc.Utf8);
-			} else {
+			} 
+			else {
 				return msg;
 			}
 		}
-		return msg;
+	
+	return msg;
   };
 }]);
