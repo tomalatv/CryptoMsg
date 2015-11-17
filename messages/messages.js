5c080cb6dd8e091e91d1a8349c393b1b0f211dc6
tomalatv <toni.latvakoski@gmail.com>
2015-11-17 14:46:40 +0200

    automatic message scorlling to last message

--- messages/messages.js
+++ messages/messages.js
@@ -9,22 +9,32 @@ angular.module('cryptoMsg.messages', ['ngRoute', 'firebase'])
     });
 }])
 
-.controller('MessagesCtrl', function($scope, $firebaseArray) {
+.controller('MessagesCtrl', function($scope, $firebaseArray, $location, $anchorScroll) {
     var url = 'https://cryptomsg.firebaseio.com/msgs';
     var firebRef = new Firebase(url);
-    $scope.e = '';
     $scope.dec = '';
 
-    $scope.msgArray = $firebaseArray(firebRef);
-
-    $scope.cryptMsg = function(msg, cryptKey) {
+    var msg = $firebaseArray(firebRef);
+    $scope.msgArray = '';
+    msg.$loaded()
+    .then(function(msgs) {
+    	console.log('on load scroll last');
+    	$scope.msgArray = msgs;
+    })
+    .catch(function(err) {
+    	console.log('messages cannot loaded !!', err);
+    });
+	/**
+	 * @param  {msg} user writen message to be crypted 
+	 * @param  {cryptKey} user seted crypting key for message
+	 * @return {void}
+	 */
+    $scope.sendCryptMsg = function(msg, cryptKey) {
         var now = moment();
         if (cryptKey !== undefined && cryptKey !== '' && msg !== undefined && msg !== '') {
             var cryptedMsg = CryptoJS.AES.encrypt(msg, cryptKey);
-            $scope.e = cryptedMsg.toString();
             var data = JSON.parse('{ "msg": "' + cryptedMsg.toString() + '", "send": "' + now + '" }');
             firebRef.push(data);
-            $scope.message = '';
         }
     }
 
@@ -38,10 +48,22 @@ angular.module('cryptoMsg.messages', ['ngRoute', 'firebase'])
             }
         }
     }
-
+    /**
+     * @param  {msgId} messages id to be removed form firebase
+     * @return {void}
+     */
     $scope.removeMsg = function(msgId) {
         console.log("REMOVE: ", msgId);
         firebRef.child(msgId).remove();
     }
+    /**
+     * scroll end of messagas when view load's last message form firebase
+     * @return {void}
+     */
+    $scope.scrollToLastMsg = function() {
+    	$location.hash("last");
+    	$anchorScroll();
+    }
+    
 
 });
