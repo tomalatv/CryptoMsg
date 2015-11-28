4455cd2c6428afc5a82458c1aac7f4e10c320710
tomalatv <toni.latvakoski@gmail.com>
2015-11-28 20:06:10 +0200

    new menu added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 angular.module('cryptoMsg', [
   'ngRoute',
   'cryptoMsg.messages',
+  'cryptoMsg.menu',
   'cryptoMsg.decrypt-filter'
 ]).
 config(['$routeProvider', function($routeProvider) {
---
+++ menu/menu.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('cryptoMsg.menu', [])
+.directive('navMenu', [ function(){
+	// Runs during compile
+	return {
+		// name: '',
+		// priority: 1,
+		// terminal: true,
+		// scope: {}, // {} = isolate, true = child, false/undefined = no change
+		// controller: function($scope, $element, $attrs, $transclude) {},
+		// require: 'ngModel', // Array = multiple requires, ? = optional, ^ = check parent elements
+		restrict: 'E', // E = Element, A = Attribute, C = Class, M = Comment
+		templateUrl: 'menu/menu.html',
+		// replace: true,
+		// transclude: true,
+		// compile: function(tElement, tAttrs, function transclude(function(scope, cloneLinkingFn){ return function linking(scope, elm, attrs){}})),
+		link: function($scope, iElm, iAttrs, controller) {
+			$scope.showMenu = false;
+		}
+	};
+}]);
