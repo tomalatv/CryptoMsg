8febea4a19be41647673130d57a915a13ccc9c56
tomalatv <toni.latvakoski@gmail.com>
2015-11-29 13:32:45 +0200

    gulp browsersync and UI tunings

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('*/*.js').on('change', browserSync.reload);
+    gulp.watch('*/*.html').on('change', browserSync.reload);
+    gulp.watch('*.css').on('change', browserSync.reload);
+    gulp.watch('*.js').on('change', browserSync.reload);
+});
+
+// gulp.task('watch', function() {
+//     gulp.watch('**/*.js', ['browser-sync']);
+//     gulp.watch('**/*.html', ['browser-sync']);
+//     gulp.watch('*.js', ['browser-sync']);
+//     gulp.watch('*.css', ['browser-sync']);
+// });
+
+gulp.task('default', ['browser-sync']);
